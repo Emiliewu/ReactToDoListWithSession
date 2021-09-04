dd1d8bc241afe27f84515488f6d95d7371661fd4
Emilie <emilieweiwu@gmail.com>
2021-09-03 18:07:25 -0700

    adding git ignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import Input from './components/Input';
+import Todo from './components/Todo';
 
 function App() {
+  const listStr = sessionStorage.getItem('todoList');
+  const listArr = listStr ? JSON.parse(listStr) : [];
+  const [list, setList] = useState(listArr);
+  const idStr = sessionStorage.getItem('id');
+  const idNum = idStr ? +idStr : 1;
+  const [id, setId] = useState(idNum);
+
+  useEffect(() => {
+    sessionStorage.setItem('todoList', JSON.stringify(list));
+  }, [list]);
+  useEffect(() => {
+    sessionStorage.setItem('id', id);
+  }, [id]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Input setList={setList} id={id} setId={setId} />
+      <div>
+        { list.map((todo, idx) => <Todo key={todo.id} idx={idx} {...todo} setList={setList} />) }
+      </div>
+    </>
   );
 }
 
 export default App;
+
